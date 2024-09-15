0397483994a531f6bd177b587101663dcdcee336
SafeDrive6 <randyaveryj897o@gmail.com>
2024-09-15 15:54:43 +0800

    Add files via upload

---
+++ index_files/js(1).php
@@ -0,0 +1,3 @@
+var htmlData = document.getElementsByTagName('html')[0];
+htmlData.setAttribute('data-guid', '705126aa-dcd1-dbc2-80b0-693f4e17cbd2');
+htmlData.setAttribute('data-country_ip', 'PH');
---
+++ index_files/js.php
@@ -0,0 +1,4 @@
+var htmlData = document.getElementsByTagName('html')[0];
+htmlData.setAttribute('data-landing_url', 'https://de10.cardiobalanceoriginal.com/');
+htmlData.setAttribute('data-guid', 'd493edb7-566d-de54-bd68-96953b371b29');
+htmlData.setAttribute('data-country_ip', 'PH');
